046225ea161e9b115401aa56b7b9576df9bde5e6
Pragathi7569 <153709557+Pragathi7569@users.noreply.github.com>
2024-08-11 14:25:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import time_series
+from unittest import main
+
+# Test your function by calling it here
+time_series.draw_line_plot()
+time_series.draw_bar_plot()
+time_series.draw_box_plot()
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
